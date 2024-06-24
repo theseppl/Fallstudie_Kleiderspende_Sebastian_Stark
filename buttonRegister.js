da3a1785d76b04a4966edd993b07cea5d6724d71
Sebastian Stark <sstark@gmx.net>
2024-06-24 21:54:20 +0200

    Ablauf für Anmeldung begonnen

--- buttonRegister.js
+++
@@ -1,6 +0,0 @@
-function register(){
-    // const registerForm = document.getElementById('Form');
-    // registerForm.innerHTML = "Hallo";
-
-    window.open('./register.html', '_self')
-}
---
+++ donate.js
@@ -0,0 +1,8 @@
+// Funktion um später das Registrierungsfenster zu öffen.
+// function register(){
+//     window.open('./register.html', '_self')
+// }
+
+function donate(){
+        window.open('./donate.html', '_self')
+    }

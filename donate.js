fbf6c0db4004ade8f02d4cf779ccdd1943b75421
Sebastian Stark <sstark@gmx.net>
2024-06-30 08:59:10 +0200

    HTML-Dateien aufgeräumt

--- donate.js
+++ donate.js
@@ -1,8 +1,3 @@
-// Funktion um später das Registrierungsfenster zu öffen.
-// function register(){
-//     window.open('./register.html', '_self')
-// }
-
 function donate(){
-        window.open('./donate.html', '_self')
+        window.open('/html/donate.html', '_self')
     }
--- fetchOrBring.js
+++ fetchOrBring.js
@@ -1,8 +1,8 @@
 let radioOne = document.getElementById("radio1");
       function fetchOrBring() {
         if (radioOne.checked) {
-          window.open('./bringRegister.html', '_self')
+          window.open('/html/bringRegister.html', '_self')
         } else {
-          window.open('./fetchRegister.html', '_self')
+          window.open('/html/fetchRegister.html', '_self')
         }
       }

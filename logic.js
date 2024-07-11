05863187913f5d88eeb379a66a259d6119424db2
Sebastian Stark <sstark@gmx.net>
2024-07-11 21:45:03 +0200

    bringRegister überflüssig!?

--- logic.js
+++ logic.js
@@ -25,7 +25,8 @@ document.addEventListener("DOMContentLoaded", function() {
 let radioOne = document.getElementById("radio1");
       function fetchOrBring() {
         if (radioOne.checked) {
-          window.open('/html/bringRegister.html', '_self')
+          // window.open('/html/bringRegister.html', '_self')
+          window.open('/html/clothes.html', '_self')
         } else {
           window.open('/html/fetchRegister.html', '_self')
         }

1a8a46aad3f38f2fc2fb2f21940cb2c9e99cf590
Sebastian Stark <sstark@gmx.net>
2024-08-18 12:53:00 +0200

    Areas angepasst

--- logic.js
+++ logic.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     var meineNavbar = document.getElementById("meine-navbar");
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", "navigation.html", true);
+    xhr.open("GET", "./navigation.html", true);
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
             meineNavbar.innerHTML = xhr.responseText;
